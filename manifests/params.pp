2e0eb3316331f16f52080fc62c487c93dbb4a1fa
David Gibbons <dag@rentrak.com>
2013-09-16 15:22:13 -0700

    lint cleanup

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,28 +1,28 @@
+# Parameters for the beanstalkd class
+#
 class beanstalkd::params {
-
-
   case $::operatingsystem {
     ubuntu,debian: {
       $defaultpackagename='beanstalkd'
       $defaultservicename='beanstalkd'
       $user='beanstalkd'
-      $configfile="/etc/default/beanstalkd"
-      $configtemplate="$module_name/debian/beanstalkd_default.erb"  #please create me!
-      $hasstatus='true'
+      $configfile='/etc/default/beanstalkd'
+      $configtemplate="${module_name}/debian/beanstalkd_default.erb"  # please create me!
+      $hasstatus=true
       $restart='/etc/init.d/beanstalkd restart'
     }
     centos,redhat: {
       $defaultpackagename='beanstalkd'
       $defaultservicename='beanstalkd'
       $user='beanstalkd'
-      $configfile="/etc/sysconfig/beanstalkd"
-      $configtemplate="$module_name/redhat/beanstalkd_sysconfig.erb"
-      $hasstatus='true'
+      $configfile='/etc/sysconfig/beanstalkd'
+      $configtemplate="${module_name}/redhat/beanstalkd_sysconfig.erb"
+      $hasstatus=true
       $restart='/etc/init.d/beanstalkd restart'
     }
-    #TODO: add more OS support!
+    # TODO: add more OS support!
     default: {
-      fail("ERROR [$module_name]: I don't know how to manage this OS: ${::operatingsystem}")
+      fail("ERROR [${module_name}]: I don't know how to manage this OS: ${::operatingsystem}")
     }
   }
 
